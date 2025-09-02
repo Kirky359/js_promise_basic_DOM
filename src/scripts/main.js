241b674580e9780f5a24ed213469e353c087ff3d
kirky359 <lol783176@gmail.com>
2025-09-02 16:30:59 +0300

    plz

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,32 @@
-'use strict';
-
 const promise1 = new Promise((resolve, reject) => {
   const logo = document.querySelector('.logo');
 
-  logo.addEventListener('click', () => {
-    try {
-      const div = document.createElement('div');
-
-      div.className = 'message';
-
-      resolve(div);
-    } catch (err) {
-      reject(err);
-    }
-  });
+  if (logo) {
+    logo.addEventListener('click', () => {
+      try {
+        resolve();
+      } catch (err) {
+        reject(err);
+      }
+    });
+  } else {
+    reject(Error);
+  }
 });
 
 promise1
-  .then((div) => {
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
     div.textContent = 'Promise was resolved!';
     document.body.appendChild(div);
   })
-  .catch((err) => {
+  .catch(() => {
     const div = document.createElement('div');
 
     div.className = 'message error-message';
-    div.textContent = `Promise failed: ${err}`;
+    div.textContent = 'Promise was rejected!';
     document.body.appendChild(div);
   });
 
