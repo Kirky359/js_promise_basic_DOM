622efb37eb1e547f39408e4199b4936a2f863e0b
kirky359 <lol783176@gmail.com>
2025-09-02 16:25:44 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,31 +4,47 @@ const promise1 = new Promise((resolve, reject) => {
   const logo = document.querySelector('.logo');
 
   logo.addEventListener('click', () => {
-    const div = document.createElement('div');
+    try {
+      const div = document.createElement('div');
 
-    div.className = 'message';
-    resolve(div);
+      div.className = 'message';
+
+      resolve(div);
+    } catch (err) {
+      reject(err);
+    }
   });
 });
 
-promise1.then((div) => {
-  div.textContent = 'Promise was resolved!';
+promise1
+  .then((div) => {
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  })
+  .catch((err) => {
+    const div = document.createElement('div');
 
-  const body = document.querySelector('body');
-
-  body.appendChild(div);
-});
+    div.className = 'message error-message';
+    div.textContent = `Promise failed: ${err}`;
+    document.body.appendChild(div);
+  });
 
 const promise2 = new Promise((resolve, reject) => {
-  const div = document.createElement('div');
-
-  div.className = 'message error-message';
-  div.textContent = 'Promise was rejected!';
-  setTimeout(reject(div), 3000);
+  setTimeout(() => reject(Error), 3000);
 });
 
-promise2.catch((div) => {
-  const body = document.querySelector('body');
+promise2
+  .then(() => {
+    const div = document.createElement('div');
 
-  body.appendChild(div);
-});
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  });
