b740c68f9e832c61c2bfdc50a910c7fbaf8a1340
kirky359 <lol783176@gmail.com>
2025-09-02 16:03:13 +0300

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    resolve(div);
+  });
+});
+
+promise1.then((div) => {
+  div.textContent = 'Promise was resolved!';
+
+  const body = document.querySelector('body');
+
+  body.appendChild(div);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = 'Promise was rejected!';
+  setTimeout(reject(div), 3000);
+});
+
+promise2.catch((div) => {
+  const body = document.querySelector('body');
+
+  body.appendChild(div);
+});
